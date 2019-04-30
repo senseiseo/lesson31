3a3ba87041528b2516bf0b80b1c4844601f073bc
ildar <1919mur@gmail.com>
2019-04-30 09:44:18 +0300

    //play with localstorage

--- public/app.js
+++ public/app.js
@@ -1,6 +1,9 @@
-var x = 1
+function something()
 
-function something() {
+{
+    var x = window.localStorage.getItem('aaa');
+
+    window.localStorage.setItem('aaa', 555);
+
     alert(x)
-    x = x + 1
 }
