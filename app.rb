cd92c15fd7ad2ec8f2ac451f4d9c5fa4606ba43e
ildar <1919mur@gmail.com>
2019-05-06 19:13:35 +0300

    //update

--- app.rb
+++ app.rb
@@ -11,13 +11,17 @@ class Product < ActiveRecord::Base
 end 
 
 get '/' do
+	
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
 get '/index' do 
 	erb :index 
+	@product = Product.all
 end 
 
 get '/about' do 
+	
+	@products = Product.all 
 	erb :about 
 end 
