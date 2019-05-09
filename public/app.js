81868939d0d78eeb05f9e3d9c8e8a1a75e9e09d6
ildar <1919mur@gmail.com>
2019-05-09 19:02:57 +0300

    /update layout.erb

--- public/app.js
+++ public/app.js
@@ -16,6 +16,7 @@ function add_to_cart(id) {
     var x = window.localStorage.getItem(key);
     x = x * 1 + 1
     window.localStorage.setItem(key, x)
+    alert('Items in your cart:' + cart_get_number_of_items());
 }
 
 function cart_get_number_of_items() {
