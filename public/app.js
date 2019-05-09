b7d0df6e9581f3ee790f0e4ee6cafe6edb036358
ildar <1919mur@gmail.com>
2019-05-09 18:42:23 +0300

    //

--- public/app.js
+++ public/app.js
@@ -17,3 +17,18 @@ function add_to_cart(id) {
     x = x * 1 + 1
     window.localStorage.setItem(key, x)
 }
+
+function cart_get_number_of_items() {
+    var cnt = 0;
+
+    for (var i = 0; i < window.localStorage.length; i++) {
+        var key = window.localStorage.key(i); // получаем ключ
+        var value = window.localStorage.getItem(key); // получаем значение
+
+        if (key.indexOf('product_') == 0) {
+            cnt = cnt + value * 1;
+        }
+    }
+
+    return cnt;
+}
