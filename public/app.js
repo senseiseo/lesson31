aa8976861e7bb3f96fb6c6b0d4d2cbdeada20da0
ildar <1919mur@gmail.com>
2019-05-10 18:41:10 +0300

    //update

--- public/app.js
+++ public/app.js
@@ -15,10 +15,11 @@ function add_to_cart(id) {
     var x = window.localStorage.getItem(key);
     x = x * 1 + 1;
     window.localStorage.setItem(key, x);
-    alert('Items in your cart: ' + cart_get_number_of_items());
+
     update_orders_input();
     update_orders_button();
 }
+
 
 function cart_get_number_of_items() {
     var cnt = 0;
@@ -40,6 +41,12 @@ function update_orders_input() {
     var orders = cart_get_orders();
     $('#orders_input').val(orders);
 
+}
+
+function update_orders_button() {
+
+    var text = 'Cart (' + cart_get_number_of_items() + ')';
+    $('#orders_button').val(text);
 }
 
 function cart_get_orders() {
